f5c28dfbe83c130fac0309c23867ac9f09e810fc
Ben Jones <ben@bnjns.uk>
2020-05-08 17:46:06 +0100

    enable cypress in CI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  reporters: ['default', 'jest-junit'],
+  reporters: [
+    'default', [
+      'jest-junit', {
+        suiteName: 'Unit Tests',
+        outputDirectory: 'tests',
+        outputName: 'unit.xml',
+        uniqueOutputName: 'false'
+      }
+    ]
+  ],
   snapshotSerializers: ['jest-serializer-vue'],
   collectCoverage: true,
   collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
