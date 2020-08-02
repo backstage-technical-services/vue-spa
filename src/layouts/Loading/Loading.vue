2e22b662b97575e355651816ae130ac331c00da9
Ben Jones <ben@bnjns.uk>
2020-08-02 22:40:20 +0100

    add better loading state

--- src/layouts/Loading/Loading.vue
+++ src/layouts/Loading/Loading.vue
@@ -1,12 +1,25 @@
 <template>
-  <h1>Loading!</h1>
+  <div id="app">
+    <font-awesome-icon :icon="spinner" spin />
+  </div>
 </template>
 
 <script lang="ts">
+import { faSpinnerThird } from '@fortawesome/pro-solid-svg-icons'
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Loading extends Vue {
-
+  private readonly spinner = faSpinnerThird
 }
 </script>
+
+<style lang="scss" scoped>
+  @import "../../assets/css/themes/general";
+
+  .svg-inline--fa {
+    flex: 1;
+    font-size: 2.5rem;
+    margin: 0 auto;
+  }
+</style>
